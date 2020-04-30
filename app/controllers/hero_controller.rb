930cb9ecb76a60d22bc0f2387e5e027fd6526a4c
Kino Kiaro <kiarokino@gmail.com>
2020-04-30 15:05:25 +0900

    2-3。ビューを記述した。スタイルシートでhtmlのスタイルを指定。

--- app/controllers/hero_controller.rb
+++ app/controllers/hero_controller.rb
@@ -1,15 +1,6 @@
 class HeroController < ApplicationController
 
   def index
-    if request.post? then
-      @title = 'Result'
-      @msg = 'you typed: ' + params['input1'] + '.'
-      @value = params['input1']
-    else
-      @title = 'index'
-      @msg = 'type text...'
-      @value = ''
-    end
   end
 
 end
